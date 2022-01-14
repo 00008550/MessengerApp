100c136b80a7f59f61476a2da196fb91ab68b2c7
00008550 <davron.yusupov.2000@gmail.com>
2022-01-14 12:05:23 +0500

    refactoring for commit: 5d645c2e6ce5bcde8776863ebeda3ef880a997d7

--- API/Extensions/AppServiceExtensions.cs
+++ API/Extensions/AppServiceExtensions.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿using BLL.Services.TokenService;
+using DAL.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace API.Extensions
 {
-    public class AppServiceExtensions
+    public static class AppServiceExtensions
     {
+        public static IServiceCollection AddAppServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddScoped<IJWTService, JWTService>();
+            services.AddDbContext<DataContext>(options =>
+            options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+            return services;
+        }
     }
 }
