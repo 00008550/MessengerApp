c85142c0f08b8871cbe698d0cd16dc6622bb2447
00008550 <davron.yusupov.2000@gmail.com>
2022-02-02 22:11:50 +0500

    error handling developed
    Developed exception middleware and client side validation to basic errors

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,48 @@
+﻿using DAL.Data;
+using DAL.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public BuggyController(DataContext context)
+        {
+            _context = context;
+        }
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecret()
+        {
+            return "secret text";
+        }
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound(Guid id)
+        {
+            var thing = _context.Users.Find(id);
+            if (thing == null) return NotFound();
+            return Ok(thing); 
+        }
+        [HttpGet("server-error")]
+        public ActionResult<string> GetServerError(Guid id)
+        {
+            var thing = _context.Users.Find(id);
+
+            var thingToReturn = thing.ToString();
+            return thingToReturn;
+        }
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest("This is not good request");
+        }
+        
+
+    }
+}
--- BLL/Models/RegisterDTO.cs
+++ BLL/Models/RegisterDTO.cs
@@ -12,6 +12,7 @@ namespace BLL.Models
         [Required]
         public string Username { get; set; }
         [Required]
+        [StringLength(8,MinimumLength=4)]
         public string Password { get; set; }
     }
 }
